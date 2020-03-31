a2e7d20c840d01ff4de4ce93b8978c3d4ff2f743
AlbertoEstep <albertoestep@gmail.com>
2020-03-31 18:32:53 +0200

    Starting P2

---
+++ P2/src/AGG_uniforme.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
+
+using namespace std;
+
+/* Rellena la matriz de distancias
+
+Parametros:
+  - m = matriz de distancias
+*/
+
+void leerDatos(vector<vector<double>> &m){
+  double distancia;
+  for (unsigned i = 1; i < m.size(); i++) {
+    for (unsigned j = i + 1; j < m.size(); j++) {
+      cin >> distancia >> distancia >> distancia;
+      m[i][j] = m[j][i] = distancia;
+    }
+  }
+}
+
+
+/* --------------------------- MAIN ---------------------------*/
+
+int main(int argc, char *argv[]){
+  int n_total, n_sel; // n_total = número de elementos &&
+                      // n_sel = el número de elementos a seleccionar del problema
+  const int MAX_EVALUACIONES = 50000;
+
+  cin >> n_total >> n_sel; // inicializo los valores con la primera línea del fichero
+  vector<double> v(n_total, 0);
+  vector<vector<double>> m(n_total, v); // m = matriz de entradas
+  leerDatos(m); // inicializo la matriz de entradas
+
+  int semilla = atoi(argv[1]);
+  // Fijamos la semilla
+  srand(semilla);
+
+  AGGuniforme(m, n_sel, MAX_EVALUACIONES); // aplico el algoritmo AGGuniforme
+}
